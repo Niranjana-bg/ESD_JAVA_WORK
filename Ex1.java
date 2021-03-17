2447f20597e516216cbe0c1c3ee5b4c4533324a2
Niranjana-bg <niranjanabg1998@gmail.com>
2021-03-17 10:32:41 +0530

    Msg1

---
+++ Ex1.java
@@ -0,0 +1,29 @@
+class P
+{
+ public static void main(String args[])
+ {
+  System.out.println("Inside P");
+  main(10);
+ }
+ 
+ public static void main(int a)
+ {
+  System.out.println("InsidePP");
+ }
+}
+
+class Q
+{
+	public static void main(String args[])
+ {
+  System.out.println("Inside Q");
+ }
+}
+
+class R
+{
+	public static void main(String args[])
+ {
+  System.out.println("Inside R");
+ }
+}
